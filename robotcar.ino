762a224c3047f2a4eec391253bbecdd4a753c5c6
Nick Walker <nick@nickswalker.com>
2014-12-04 19:26:26 -0600

    Cleanup style
    Add comments
    Add motor disable to allStop for better stopping action.

--- robotcar.ino
+++ robotcar.ino
@@ -24,6 +24,8 @@ void setup() {
   pinMode(FORWARD2, OUTPUT);
   pinMode(BACKWARD1, OUTPUT);
   pinMode(BACKWARD2, OUTPUT);
+  digitalWrite(ENABLE1, HIGH);
+  digitalWrite(ENABLE2, HIGH);
  Serial.begin(9600);
 }
 
@@ -35,9 +37,8 @@ int rightBumper;
 int leftBumper;
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(ENABLE1, HIGH);
-  digitalWrite(ENABLE2, HIGH);
 
+  //BEGIN MEASUREMENTS
   leftPhotoReading = analogRead(LEFTPHOTO);
   middlePhotoReading = analogRead(CENTERPHOTO);
   rightPhotoReading = analogRead(RIGHTPHOTO); 
@@ -45,15 +46,25 @@ void loop() {
   rightBumper = analogRead(RIGHTBUMPER);
   frontBumper = analogRead(FRONTBUMPER);
   logReadings();
+ 
+  enableMotors();
+  //BEGIN ALGORITHM 
+  
+  //If the car cannot move forward it will stop. The user must remove the obstacle
+  //manually.
   if(frontIsObstructed()){
     allStop();
   }
+  //If both the left and right are obstructed, there's only one direction we
+  //can go in
   else if(leftIsObstructed() && rightIsObstructed()){
     forward();
   }
+  //We're going to assume the line continues in the same direction.
   else if(lineDetected()){
     forward();
-    delay(STEP_DELAY);
+    //we only want to leave the forward motion engaged for a short time.
+    
   }
   else if(shouldTurnLeft()){
     left();
@@ -61,48 +72,59 @@ void loop() {
   else{
     right();
   }
-  delay(1);
+  //No matter what state we ended up in, we want to allow it to persist
+  //for a few milliseconds.
+  delay(STEP_DELAY);
+  //After the delay, we'll stop...
   allStop();
+  //and give the ADC a breather so we can get fresh readings for the next loop.
   delay(600);
 }
 
 void forward(){
-    digitalWrite(FORWARD1, HIGH);
+  digitalWrite(FORWARD1, HIGH);
   digitalWrite(FORWARD2, HIGH);
   digitalWrite(BACKWARD1, LOW);
   digitalWrite(BACKWARD2, LOW);
 }
-void stop(){}
 
 void backward(){
   digitalWrite(FORWARD1, LOW);
   digitalWrite(FORWARD2, LOW);
-    digitalWrite(BACKWARD1, HIGH);
+  digitalWrite(BACKWARD1, HIGH);
   digitalWrite(BACKWARD2, HIGH);
 }
 
 void left(){
-    digitalWrite(FORWARD1, LOW);
+  digitalWrite(FORWARD1, LOW);
   digitalWrite(FORWARD2, HIGH);
-    digitalWrite(BACKWARD1, HIGH);
+  digitalWrite(BACKWARD1, HIGH);
   digitalWrite(BACKWARD2, LOW);
 }
 
 void right(){
-    digitalWrite(FORWARD1, HIGH);
+  digitalWrite(FORWARD1, HIGH);
   digitalWrite(FORWARD2, LOW);
-    digitalWrite(BACKWARD1, LOW);
+  digitalWrite(BACKWARD1, LOW);
   digitalWrite(BACKWARD2, HIGH);
 }
 void allStop(){
   digitalWrite(FORWARD1, LOW);
   digitalWrite(FORWARD2, LOW);
-    digitalWrite(BACKWARD1, LOW);
+  digitalWrite(BACKWARD1, LOW);
   digitalWrite(BACKWARD2, LOW);
+  digitalWrite(ENABLE1, LOW);
+  digitalWrite(ENABLE2, LOW);
+}
+void enableMotors(){
+  digitalWrite(ENABLE1, HIGH);
+  digitalWrite(ENABLE2, HIGH); 
 }
 
 boolean lineDetected(){
   boolean detected = (middlePhotoReading < rightPhotoReading) && (middlePhotoReading < leftPhotoReading);
+  //We'll set this indicator so that we can tell the state of the 
+  //detection externally.
   if(detected){
     digitalWrite(LED, HIGH);
   }
@@ -128,12 +150,13 @@ boolean shouldTurnRight(){
 }
 void logReadings(){
   Serial.println("***");
+  Serial.println("PHOTORESISTORS: L M R");
   Serial.println(leftPhotoReading);
   Serial.println(middlePhotoReading);
   Serial.println(rightPhotoReading);
-  Serial.println("BUMPERS");
+  Serial.println("BUMPERS: L F R");
   Serial.println(leftBumper);
-  Serial.println(rightBumper);
   Serial.println(frontBumper);
+  Serial.println(rightBumper);
   delay(100);
 }
